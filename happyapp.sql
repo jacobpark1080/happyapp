c00680718fdccdee8c1e7175d661957587dc9af7
Zaaler <zachary.allen@colorado.edu>
2015-10-11 17:22:11 -0600

    All restaurants and 8 of monday happy specials input

--- happyapp.sql
+++ happyapp.sql
@@ -4,12 +4,10 @@ USE `happyapp`;
 
 CREATE TABLE IF NOT EXISTS `monday` (
   `restID` int(11) DEFAULT NULL,
-  `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `stime` decimal(10,2) DEFAULT NULL,
   `etime` decimal(10,2) DEFAULT NULL,
   `drink` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
   `food` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `monday` (`restID`, `name`, `address`, `stime`, `etime`, `drink`, `food`) VALUES(101, 'Sundown Saloon', 'address', 18.00, 19.00, 'drink','food');
+INSERT INTO `monday` (`restID`, `stime`, `etime`, `drink`, `food`) VALUES(101, 18.00, 19.00, 'drink','food');
