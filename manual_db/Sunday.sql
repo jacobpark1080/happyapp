13c9dff44f1a27d769e56d47d4bce9d56a63cf01
Zaaler <zachary.allen@colorado.edu>
2015-10-12 15:23:29 -0600

    Creating directory for manual database subsection

---
+++ manual_db/Sunday.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: 
+-- Date: 2015-10-11 18:08
+*/
+INSERT INTO `Sunday` (`Restaurant_restname`,`stime`,`etime`,`food`,`drink`) VALUES ('Boulder Chophouse & Tavern','16','18','$10 Early Buff Special','$3 Draft Beer, $4 Glass of Wine and Well Drinks, $5 Specialty Cocktails');
+INSERT INTO `Sunday` (`Restaurant_restname`,`stime`,`etime`,`food`,`drink`) VALUES ('Centro Latin Kitchen','9.5','11.5','Brunch, $2-$4 Plates, $3 Tacos','$2 Cuba Libres, $3 Ska Mexican Logger, La Casa Marg, Bloody Marys and Mimosas, $4 Gold Coin Margarita');
+INSERT INTO `Sunday` (`Restaurant_restname`,`stime`,`etime`,`food`,`drink`) VALUES ("Illegal Pete's",'15','20','Free Chips and Salsa for Drinkers','$2.50 Coors Light and Draft, $3 Wells, $3.50 Select Craft Drafts, $4 House Margaritas');
+INSERT INTO `Sunday` (`Restaurant_restname`,`stime`,`etime`,`food`,`drink`) VALUES ('Salt','15','18','$1-$6 Appetizers','$3 Select Beers, $4 Drafts and House Wine, $4/$5 Cocktails');
+INSERT INTO `Sunday` (`Restaurant_restname`,`stime`,`etime`,`food`,`drink`) VALUES ('Sushi Hana','16','17.5','$2.95 Little Side Dishes, $7 2 Rolls','$3-$3.50 Beer, $3.50 Gekkeikan Sake, $3.50-$4.50 Cocktails');
+INSERT INTO `Sunday` (`Restaurant_restname`,`stime`,`etime`,`food`,`drink`) VALUES ('28th Street Tavern','0','24','None','$1 off Drafts/Wells');
+INSERT INTO `Sunday` (`Restaurant_restname`,`stime`,`etime`,`food`,`drink`) VALUES ('Absinthe House','16','19','None','$1 Session Lager Bottle, Bud, and Bud Light, $3 Mixed Cocktails and Summer Sangria');
+INSERT INTO `Sunday` (`Restaurant_restname`,`stime`,`etime`,`food`,`drink`) VALUES ('Agave','16.5','18','$5-$6 Select Appertizers','$5 House Margaritas, $6 Agave Coin Margaritas');
