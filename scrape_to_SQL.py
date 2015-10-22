6f6660bffe4d0f9a08b2cf62167e2f65df1f8ea3
sniemeyer13 <sniemeyer13@gmail.com>
2015-10-22 09:25:22 -0600

    almost done modifying times to put into database, having trouble with Resturaunts (i.e. Eggloo) which have multiple start and end times. Need to talk with group to see how to address this in database

--- scrape_to_SQL.py
+++ scrape_to_SQL.py
@@ -1,5 +1,7 @@
-#how to import this into the database
-#how to separate start and end times
+#do every day (not just tuesday)
+#import into database
+#figure out what to do with multiple stimes and etimes
+#insert specials
 #get address information
 
 import requests
@@ -9,7 +11,9 @@ import MySQLdb
 
 
 gmap = {}
-
+timemap = {}
+drinkmap = {}
+foodmap = {}
 #tuesday
 url = "http://www.therooster.com/happyhours/boulder#d=tuesday"
 r = requests.get(url)
@@ -24,10 +28,10 @@ for item in tueInfo:
 		gmap[i] = restname
 	except:
 		print "got an error"
-	tueTime = item.contents[5].find_all("div", {"class" : "time"})[0].text
-	tueDrinks = item.contents[5].find_all("div", {"class" : "foods"})[0].find_all("div", {"class": "food"})[0].text.replace('Drinks:', '')
+	timemap[i] = item.contents[5].find_all("div", {"class" : "time"})[0].text
+	drinkmap[i] = item.contents[5].find_all("div", {"class" : "foods"})[0].find_all("div", {"class": "food"})[0].text.replace('Drinks:', '')
 	try:
-		tueFood = item.contents[5].find_all("div", {"class" : "foods"})[0].find_all("div", {"class": "food"})[1].text.replace('Food:', '')
+		foodmap[i] = item.contents[5].find_all("div", {"class" : "foods"})[0].find_all("div", {"class": "food"})[1].text.replace('Food:', '')
 	except:
 		pass
 	i=i+1
@@ -35,6 +39,9 @@ for item in tueInfo:
 addrest = ("INSERT INTO Restaurant"
 			  "(restname, address)" 
 			  "VALUES (%s, %s)")
+add_special = ("INSERT INTO Tuesday" 
+			"(stime, etime,food,drink)"
+			"VALUES (%s, %s, %s, %s)")
 
 
 
@@ -42,11 +49,43 @@ conn = MySQLdb.connect(user="root",db="mydb")
 x = conn.cursor()
 
 for key in gmap.keys():
-	print gmap[key]	
+	
 	name = gmap[key]
 	add = "temp"
 	datarest = (str(name),str(add))
-	x.execute(addrest, datarest)
+	try:
+		x.execute(addrest, datarest)
+	except:
+		pass
+
+	time = timemap[key]
+	drink = drinkmap[key]
+	try:
+		food = foodmap[key]
+	except:
+		pass
+	
+	time = time.split('-',1)
+	stime = time[0]
+	try:
+		etime = time[1]
+	except:
+		stime = "0:00"
+		etime = "24:00"
+	st = stime.replace("pm","")	
+	st = st.replace(":00"," ")
+	st = st.replace(":30",".5")
+	et = etime.replace("pm","")
+	et = et.replace(":00"," ")
+	et = et.replace(":30",".5")
+	
+	#data_special = (str(st), str(et), str(drink), str(food))
+	try:
+		#x.execute(add_special,data_special)
+		pass
+	except:
+		pass
+
 x.close()
 conn.commit()
 conn.close()
